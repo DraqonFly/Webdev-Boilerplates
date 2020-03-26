6d97a31087da808a0c73681e259b0cbd844335aa
DraqonFly <DraqonFly@github.com>
2020-03-26 23:29:46 +0100

    removed frontend folder

--- frontend/src/js/index.js
+++
@@ -1,6 +0,0 @@
-import "../scss/index.scss";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-
-ReactDOM.render(<App />, document.getElementById("root"))
